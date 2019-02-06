d7085d623d10e74e953a47113344333fac377109
Karl Ho <karlho@gmail.com>
2019-02-06 14:16:00 -0600

    Add files via upload

--- workshops/R/Intro/introR01.R
+++ workshops/R/Intro/introR01.R
@@ -34,3 +34,30 @@ ls()
 # Remove objects
 
 rm(x, y)
+
+## pch = 0,square
+## pch = 1,circle
+## pch = 2,triangle point up
+## pch = 3,plus
+## pch = 4,cross
+## pch = 5,diamond
+## pch = 6,triangle point down
+## pch = 7,square cross
+## pch = 8,star
+## pch = 9,diamond plus
+## pch = 10,circle plus
+## pch = 11,triangles up and down
+## pch = 12,square plus
+## pch = 13,circle cross
+## pch = 14,square and triangle down
+## pch = 15, filled square
+## pch = 16, filled circle
+## pch = 17, filled triangle point-up
+## pch = 18, filled diamond
+## pch = 19, solid circle
+## pch = 20,bullet (smaller circle)
+## pch = 21, filled circle blue
+## pch = 22, filled square blue
+## pch = 23, filled diamond blue
+## pch = 24, filled triangle point-up blue
+## pch = 25, filled triangle point down blue
